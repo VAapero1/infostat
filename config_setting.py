718f425ff728d27133085f7e2c300d1d504d10e4
Vladislav <vladislav>
2023-12-20 14:52:10 +0300

    конфинг

--- config_setting.py
+++ config_setting.py
@@ -1,15 +1,7 @@
 settings = {
-    "APP_PATH": "/home/apps/relevanterbi4",
-    "MODEL": "gpt-3.5-turbo-16k",
     "OLIVIABI4_API_HOST": "https://api.olivia.bi4.su/",
-    "OLIVIA_API_KEY": "H6dCT69oHZgBVj1T6MrvpeVFyttZ3DCoJTgCKjNSBk3N0jXRvArHY4HSYxB46xGz",
-    "API_KEY_PROD": "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",
     "logpath": "/home/apps/relevanterbi4/relevanter_vol2/loginfo",
     "appPath": "/home/apps/relevanterbi4/relevanter_vol2",
     "app_ACC": "/home/apps/relevanterbi4/all_acc/account",
     "account_list": "https://api.olivia.bi4.su/v1/accounts/list",
-    "API_KEY_ML": "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",
-    "ORG_KEY_PROD": "org-xT0J51uMLU7ups5iNwvDUOft",
-    "DaData_api": "8e993e18b5e7e85f7ace732b5ee1381e0b4eff8a",
-    "ORG_KEY_ML": "org-GAuTQGuiAs5v4KWpXgo2zR0j"
           }
