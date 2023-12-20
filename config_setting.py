1cc4d15363e7d2755c7e97244ebc88e8463e7c2a
Vladislav <vladislav>
2023-12-20 14:54:08 +0300

    confing

--- config_setting.py
+++ config_setting.py
@@ -1,7 +1,3 @@
 settings = {
     "OLIVIABI4_API_HOST": "https://api.olivia.bi4.su/",
-    "logpath": "/home/apps/relevanterbi4/relevanter_vol2/loginfo",
-    "appPath": "/home/apps/relevanterbi4/relevanter_vol2",
-    "app_ACC": "/home/apps/relevanterbi4/all_acc/account",
-    "account_list": "https://api.olivia.bi4.su/v1/accounts/list",
           }
